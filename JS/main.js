6c990aa524984c7393809a307a98a4df5ba288e3
DEADLocked90000 <michael.h.gevorkyan@gmail.com>
2023-10-06 10:14:00 -0700

    Code Looks better
    so much indenting

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,13 @@
 /* personal project JS, placeholder */
 
-
 var themeint = sessionStorage.getItem("themeint")
 var root = document.querySelector(':root');
+
 if(themeint == null){
     var themeint = Math.floor(Math.random()*1);
     sessionStorage.setItem("themeint", themeint);
 }
+
 var colors = {
     0:{
         "main-color": "#ee8de6",
@@ -17,4 +18,3 @@ var colors = {
 var theme = colors[themeint];
 root.style.setProperty('--main-color', theme["main-color"]);
 root.style.setProperty('--highlight-color', theme["highlight-color"]);
-
